bc51c0fa8f8b9f129f79e8c5f4df4646986f5e0f
Ldoty07 <ldoty2@lakelandcollege.edu>
2026-04-02 13:58:50 -0500

    added bullet firing

--- settings.py
+++ settings.py
@@ -8,3 +8,10 @@ class Settings:
         self.bg_color = (21, 84, 148)
 
         self.ship_speed = 1.5
+
+
+        # Bullet Settings
+        self.bullet_speed = 2
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (255, 255, 255)
